9b02d0ea5f55758dfb9a786527fb63dc9bb01b9f
Alaina Traxler and Larry Tunseth <alaina@>
2016-08-09 13:53:27 -0700

    add a highlight functionality

--- js/scripts.js
+++ js/scripts.js
@@ -14,11 +14,21 @@ $(document).ready(function() {
     $("body").addClass("red-background");
   });
 
+  $("#paragraph01").click(function() {
+    $(this).addClass("purple-background");
+  });
+
+  $("#paragraph02").click(function() {
+    $(this).addClass("blue-background");
+  });
+
   $("button#invert").click(function() {
+
     if($("body").css("background-color") === "rgb(0, 0, 0)"){
       $("body").removeClass();
     } else{
       $("body").addClass("dark-background");
-    }
+      }
   });
+
 });
