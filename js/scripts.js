4cda8d82a09f69eadc3d61322d60e2756649699e
Alaina Traxler and Larry Tunseth <larry@>
2016-08-09 09:37:38 -0700

    suddenly finish

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#invert").click(function() {
+    if($("body").css("background-color") === "rgb(0, 0, 0)"){
+      $("body").removeClass();
+    } else{
+      $("body").addClass("dark-background");
+    }
+  });
+});
